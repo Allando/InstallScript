9026232f3870d0f7ba8fa8b2fdc52adda1b63d0b
ThatRobotMonkey <alla2634@edu.easj.dk>
2018-10-23 00:25:03 +0200

    lesser changes

--- install.sh
+++ install.sh
@@ -4,55 +4,57 @@ set -e
 
 verifyDistro()
 {
-	currentDistro=$(lsb_release -si)
+    currentDistro=$(lsb_release -si)
 
-	case currentDistro in
-		"ManjaroLinux" 	):	currentDistro="manjaro";;
-		"ArchLinux" 	): 	currentDistro="arch";;
-		"ubuntu" 		): 	currentDistro="ubuntu";;
-		"kali"			):	currentDistro="kali";;
-		* ): 				echo "System not supported" && (exit 1);
-	esac
+    case currentDistro in
+        "ManjaroLinux"  ):  currentDistro="manjaro";;
+        "ArchLinux"     ):  currentDistro="arch";;
+        "ubuntu"        ):  currentDistro="ubuntu";;
+        "kali"          ):  currentDistro="kali";;
+        * ):                echo "System not supported" && (exit 1);
+    esac
 
-	return currentDistro
+    return currentDistro
 }
 
 installSystemDependentStuff()
 {
-	distro=$(verifyDistro)
+    distro=$(verifyDistro)
 
-	case currentDistro in
-		"manjaro" 	): 	$(sudo arch/manjaro.sh);;
-		"arch" 		):	$(sudo arch/arch.sh);;
-		"ubuntu" 	): 	$(sudo debian/ubuntu.sh);;
-		"kali"		):	$(sudo debian/kali.sh);;
-		* ):			echo "Still not supported... How the hell did this slip through" && (exit 1);
-	esac
+    case currentDistro in
+        "manjaro"   ):  $(sudo arch/manjaro.sh);;
+        "arch"      ):  $(sudo arch/arch.sh);;
+        "ubuntu"    ):  $(sudo debian/ubuntu.sh);;
+        "kali"      ):  $(sudo debian/kali.sh);;
+        * ):            echo "Still not supported... How the hell did this slip through" && (exit 1);
+    esac
 }
 
 # Bootloader stuff
 setUpSystems()
 {
-	pathToGrub = "/usr/default/grub"
+    pathToGrub = "/usr/default/grub"
 
-	tar -xvf libs/img.tar
-	sudo cp libs/RuinerBooting.jpg /usr/share/grub/RuinerBooting.jpg
-	sudo echo GRUB_COLOR_NORMAÆ="red/black" >> $pathToGrub
-	sudo echo GRUB_COLOR_HIGHLIGHT="black/red" >> $pathToGrub
-	sudo echo GRUB_BACKGROUND="/sr/share/grub/RuinerBooting.jpg" >> $pathToGrub
+    tar -xvf libs/img.tar
+    sudo cp libs/RuinerBooting.jpg /usr/share/grub/RuinerBooting.jpg
+    sudo echo GRUB_COLOR_NORMAÆ="red/black" >> $pathToGrub
+    sudo echo GRUB_COLOR_HIGHLIGHT="black/red" >> $pathToGrub
+    sudo echo GRUB_BACKGROUND="/sr/share/grub/RuinerBooting.jpg" >> $pathToGrub
 
-	sudo systemctl enable ufw
+    sudo systemctl enable ufw
 
-	
+    
 # TODO: Cronjob: Prepare auto update for clamav
 }
 
 main()
 {
-	verifyDistro
-	installSystemDependentStuff
+    verifyDistro
+    installSystemDependentStuff
 
-	if [[ $(verifyDistro) != "kali" ]]; then
-		setUpSystems
-	fi
+    if [[ $(verifyDistro) != "kali" ]]; then
+        setUpSystems
+    fi
 }
+
+main

74e8247c2f629696b009359bd0b145a4cca88500
ThatRobotMonkey <alla2634@edu.easj.dk>
2018-10-23 00:33:39 +0200

    fixes again to kali

--- debian/kali.sh
+++ debian/kali.sh
@@ -41,9 +41,7 @@ CreatingDirectories()
 	echo $(tput setaf 6)'Creating Directories'$(tput sgr0)
 
 	#Programming directories
-	mkdir -p CTF/Ŕoot-Me \
-	Scripts/Repositories \
-	Scripts/LocalScripts
+	mkdir -p CTF/Ŕoot-Me Scripts/Repositories Scripts/LocalScripts
 
 	echo $(tput setaf 2)'Done!'$(tput sgr0)
 }
